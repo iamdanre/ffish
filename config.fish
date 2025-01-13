c8fce44fdd9945487d864507d9594673a4a9390e
danré <iamdanre@gmail.com>
2025-01-13 14:19:49 +0200

    upz

--- config.fish
+++ config.fish
@@ -8,7 +8,7 @@ end
 function fish_greeting
     if not set -q fish_greeting
         set -l line1 (printf (_ 'welcome to %sffish%s, the fucked friendly interactive shell') (set_color white) (set_color normal))
-        set -l line2 \n(printf (_ 'type %shelp%s for instructions on how to use fish %s≽⋆f< /ᐷ%s') (set_color green) (set_color normal) (set_color brmagenta))
+        set -l line2 \n(printf (_ 'type %shelp%s for instructions on how to. ffuck. ffish %s≽⋆f< /ᐷ%s') (set_color green) (set_color normal) (set_color brmagenta))
         set -g fish_greeting "$line1$line2"
     end
 
@@ -21,21 +21,17 @@ function fish_greeting
         end
     end
 
-    # Lando
     # Keep it that way to not print superfluous newlines on old configuration
     test -n "$fish_greeting"
     and echo $fish_greeting
 end
 
-#  SETUVAR fish_greeting
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# Created by `pipx` on 2024-04-01 00:22:04
-# set PATH $PATH /Users/o.x.o/.local/bin
-
 fish_add_path -U $HOME/Library/Application\ Support/Herd/bin/
+# fish_add_path -U $HOME/Library/Application\ Support/Herd/config/nvm/versions/node/v20.18.0/bin
 
 # Load fish provided completions
 if test -d ~/.config/fish/completions
@@ -44,32 +40,15 @@ if test -d ~/.config/fish/completions
     end
 end
 
-# herd completion fish | source
-# source /Users/o.x.o/.config/fish/completions/herd.fish
+eval "$(/Users/booboo/Library/Application\ Support/Herd/bin/herd.phar completion fish)"
 
-# source (herd completion fish | psub)
+source (/opt/homebrew/bin/starship init fish --print-full-init | psub)
+
+test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell_integration.fish
 
 # pnpm
-# set -gx PNPM_HOME "/Users/o.x.o/Library/pnpm"
-# if not string match -q -- $PNPM_HOME $PATH
-#   set -gx PATH "$PNPM_HOME" $PATH
-# end
+set -gx PNPM_HOME "/Users/booboo/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
 # pnpm end
-
-
-# test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
-
-#starship init fish | source
-
-
-# export PATH="/Users/o.x.o/.lando/bin${PATH+:$PATH}"; #landopath
-
-# Wasmer
-# export WASMER_DIR="/Users/o.x.o/.wasmer"
-# [ -s "$WASMER_DIR/wasmer.sh" ] && source "$WASMER_DIR/wasmer.sh"
-
-# source /opt/homebrew/opt/asdf/libexec/asdf.fish
-# uv generate-shell-completion fish | source
-# uvx --generate-shell-completion fish | source
-
-# eval (/bin/bash '/Users/o.x.o/.config/fish/themes/TinaciousDesignDark.sh')
---
+++ functions/node_zap.fish
@@ -0,0 +1,3 @@
+function node_zap --description 'Easily find and remove old and heavy node_modules folders ✨'
+    npx npkill
+end
